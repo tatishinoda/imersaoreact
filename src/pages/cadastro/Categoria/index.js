62a82b64ec32ad2c39ac7ef058ae71fedbf96868
Tati Shinoda <shinoda_@hotmail.co.uk>
2020-07-29 20:13:13 -0300

    start form

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import PageDefault from '../../../components/PageDefault';
-import { Link } from 'react-router-dom';
+import React from "react";
+import PageDefault from "../../../components/PageDefault";
+import { Link } from "react-router-dom";
 
 function CadastroCategoria() {
-	return (
-		<PageDefault>
-            <h1>Cadastro Categoria</h1>
+  return (
+    <PageDefault>
+      <h1>Cadastro Categoria</h1>
 
-            <Link to="/">
-               Voltar para Home
-            </Link>
-        </PageDefault>
-	)
+      <form>
+        <label>
+          Nome da Categoria:
+          <input type="text" />
+        </label>
+
+        <button>Cadastrar</button>
+      </form>
+
+      <Link to="/">Voltar para Home</Link>
+    </PageDefault>
+  );
 }
 
 export default CadastroCategoria;
